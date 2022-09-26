bed9c3f005946627f105c0d358e33a376eb6e35e
mostafijurmrs <mostafijurrahmanmrs241@gmail.comm>
2022-09-26 15:44:53 +0600

    added a new cart componant

---
+++ src/components/Product/Product.js
@@ -0,0 +1,26 @@
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import './Product.css';
+
+const Product = (props) => {
+    const { name, price, img, ratings, seller} = props.product;
+   
+    return (
+        <div className="product">
+            <img src={img} alt="" />
+            <div className="product-info">
+                <p className="product-name">{name}</p>
+                <p >Price: ${price}</p>
+                <p ><small>Seller: {seller}</small></p>
+                <p ><small>Ratings: {ratings} stars</small></p>
+            </div>
+            <button onClick={() => props.handleAddToCart(props.product)} className="btn-cart">
+                <p>Add To Cart</p>
+              <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+            </button>
+        </div>
+    );
+};
+
+export default Product;
